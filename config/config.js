5ff43845f553e8cfd65efb3dfcf524507625b23d
nico <1256346881@qq.com>
2024-07-19 13:54:51 +0800

    s

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
-// 配置文件
+// config/config.js
 module.exports = {
   DBHOST: "127.0.0.1",
   DBPORT: 27017,
   DBNAME: "questionStar",
-  secret: "nico",
+  DBUSER: "nico",  // 添加的用户名
+  DBPASS: "123456",  // 添加的密码
 };
--- db/db.js
+++ db/db.js
@@ -10,17 +10,24 @@ module.exports = function (success, error) {
       console.log("连接失败~~~");
     };
   }
+
   //1. 安装 mongoose
   //2. 导入 mongoose
   const mongoose = require("mongoose");
   //导入 配置文件
-  const { DBHOST, DBPORT, DBNAME } = require("../config/config.js");
+  const { DBHOST, DBPORT, DBNAME, DBUSER, DBPASS } = require("../config/config.js");
 
   //设置 strictQuery 为 true
   mongoose.set("strictQuery", true);
 
-  //3. 连接 mongodb 服务                        数据库的名称
-  mongoose.connect(`mongodb://${DBHOST}:${DBPORT}/${DBNAME}`);
+  //构建连接字符串，包括认证信息
+  const connectionString = `mongodb://${DBUSER}:${DBPASS}@${DBHOST}:${DBPORT}/${DBNAME}`;
+
+  //3. 连接 mongodb 服务
+  mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
   //4. 设置回调
   // 设置连接成功的回调  once 一次   事件回调函数只执行一次
@@ -29,8 +36,8 @@ module.exports = function (success, error) {
   });
 
   // 设置连接错误的回调
-  mongoose.connection.on("error", () => {
-    error();
+  mongoose.connection.on("error", (connError) => {
+    error(connError);
   });
 
   //设置连接关闭的回调
